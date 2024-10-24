c44017b087311c112f166599e272088b0b83f862
Kalle <alpeusk@gmail.com>
2024-10-24 13:46:00 +0200

    Added a searchbar to searchPage

--- eventure/src/pages/Searchpage/Searchpage.js
+++ eventure/src/pages/Searchpage/Searchpage.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Searchbar from '../../components/SearchpageComponents/Searchbar'
 
 const Searchpage = () => {
   return (
     <div>
       <h1>Searchpage</h1>
+      <Searchbar/>
     </div>
   )
 }
