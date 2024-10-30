36e58418de2d34e343c20a87e91a4f693fce98de
Beowulf1337 <oliver.mantorp@gmail.com>
2024-10-30 12:50:18 +0100

    fixed css to allow for later edits in code which would allow one to add and remove employees via strapi

--- eventure/src/components/AboutpageComponents/EmployeeList.jsx
+++ eventure/src/components/AboutpageComponents/EmployeeList.jsx
@@ -6,7 +6,7 @@ const EmployeeList = () => {
     { name: 'Maria', title: 'IT Manager', imageSrc: '/employeeAvatar1.png', alt: 'Kalle, IT Manager at Eventure' },
     { name: 'Helen', title: 'Product Manager', imageSrc: '/employeeAvatar2.png', alt: 'Linus, Product Manager at Eventure' },
     { name: 'Lucas', title: 'Quality Assurance', imageSrc: '/employeeAvatar3.png', alt: 'Oliver, Quality Assurance at Eventure' },
-    { name: 'Tommy', title: 'Software Architect', imageSrc: '/employeeAvatar4.png', alt: 'Tim, Software Architect at Eventure' },
+    { name: 'Tommy', title: 'Software Architect', imageSrc: '/employeeAvatar4.png', alt: 'Tim, Software Architect at Eventure' },    
   ];
 
   return (
