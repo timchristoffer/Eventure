b0609ddf4b2fd5c3e41e4aa94bd9a7cddc104855
timchristoffer <timdaahl@gmail.com>
2024-11-01 09:43:40 +0100

    Added fixes to design

--- eventure/src/components/Header/HomeHeader.jsx
+++ eventure/src/components/Header/HomeHeader.jsx
@@ -1,19 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './HomeHeader.css';
-import InnerImage from '../../assets/header/inner-image.jpg';
 
 const HomeHeader = () => {
+  const [headerData, setHeaderData] = useState({
+    headerImage: '',
+    headerTitle: '',
+    headerInfo: ''
+  });
+
+  useEffect(() => {
+    const fetchHeaderData = async () => {
+      try {
+        const response = await axios.get('http://localhost:1337/api/header?populate=*');
+        console.log('API response:', response.data);
+
+        console.log('Full Response:', response);
+
+        if (response.data && response.data.data) {
+          const header = response.data.data;
+          console.log('Header:', header);
+
+          const headerInfoHtml = header.headerInfo.map(block => {
+            if (block.type === 'paragraph') {
+              return block.children.map(child => child.text).join(' ');
+            }
+            return '';
+          }).join(' ');
+
+          setHeaderData({
+            headerImage: header.headerImage?.url ? `http://localhost:1337${header.headerImage.url}` : '',
+            headerTitle: header.headerTitle || 'Eventure',
+            headerInfo: headerInfoHtml || "Let's go on an Eventure!"
+          });
+        } else {
+          console.error('Header data is not in the expected format:', response.data);
+        }
+      } catch (error) {
+        console.error('Error fetching header data:', error);
+      }
+    };
+
+    fetchHeaderData();
+  }, []);
+
   return (
     <div className='headerContainer'>
       <div className='headerContent'>
         <div className='headerImage'>
-          <img src={InnerImage} alt='Inner' />
+          {headerData.headerImage && <img src={headerData.headerImage} alt='Header' />}
         </div>
         <div className='headerText'>
-          <h1>Eventure</h1>
-          <h3>Let's go on an Eventure!</h3>
-          <p>Find your next event</p>
-          <p>Host your own event</p>
+          <h1>{headerData.headerTitle}</h1>
+          <p dangerouslySetInnerHTML={{ __html: headerData.headerInfo }} />
+          <div className='headerButton'>
+            <a href='/search'>Find Events</a>
+            <a href='/about'>Host Events</a>
+          </div>
         </div>
       </div>
     </div>
